3d4169b8ac370ff3e35f9251b18a0048e03d4208
ShiinaOrez <shiina_orez@qq.com>
2018-03-31 20:40:05 +0800

    xxx

--- asyncio_hello.py
+++ asyncio_hello.py
@@ -1,12 +1,14 @@
+# -*- coding:UTF-8 -*-
+
 import asyncio
 
-@asyncio.coroutine
+@asyncio.coroutine # 将generator标记为coroutine类型
 def hello():
-    print("hello,world!")
-    r=yield from asyncio.sleep(1)
+    print("hello,world!") 
+    r=yield from asyncio.sleep(1) # yield from异步调用async.sleep()函数[也是一个generator]
     print("hello,again!")
 
-loop=asyncio.get_event_loop()
+loop=asyncio.get_event_loop() # 搞到一个Eventloop
 
-loop.run_until_complete(hello())
-loop.close()
+loop.run_until_complete(hello()) # 扔进去
+loop.close() # 记得关掉
